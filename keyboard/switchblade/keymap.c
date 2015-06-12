45a25076ec9aca4da835f2d781daa7a54f3e8766
Tyler Benziger <tabenziger@gmail.com>
2015-06-12 09:33:49 -0700

    Removing media

--- keyboard/switchblade/keymap.c
+++ keyboard/switchblade/keymap.c
@@ -6,7 +6,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            PGDN, TAB ,  Q   , W   , E   , R    , T    , NO  , NO     , Y      , U      , I   , O   , P   , LBRC, RBRC, BSLS, NO  , \
            HOME, LCTL,  A   , S   , D   , F    , G    , NO  , NO     , H      , J      , K   , L   , SCLN, QUOT, NO  , ENT , NO  , \
            END , LSFT,  Z   , X   , C   , V    , NO   , NO  , NO     , B      , N      , M   , COMM, DOT , SLSH, UP  , RSFT, NO  , \
-           INS , FN0 ,  LALT, LGUI, SPC , VOLUP, VOLDN, MUTE, MS_BTN1, MS_BTN3, MS_BTN2, NO  , SPC , RGUI, LEFT, DOWN, RGHT, NO  ),
+           INS , FN0 ,  LALT, LGUI, SPC , NO   , NO   , NO  , MS_BTN1, MS_BTN3, MS_BTN2, NO  , SPC , RGUI, LEFT, DOWN, RGHT, NO  ),
 
     [1] =
     KEYMAP(TRNS, ESC , F1   , F2  , F3  , F4   , F5   , TRNS, TRNS   , F6     , F7     , F8  , F9  , F10 , F11 , F12 , F13 , DEL , \
