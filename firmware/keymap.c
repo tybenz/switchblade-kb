1e0b96eb12f7b6468f9417205e3e4580579bd395
Tyler Benziger <tabenziger@gmail.com>
2015-06-15 13:51:41 -0700

    Fixing matric for more practical wiring

--- firmware/keymap.c
+++ firmware/keymap.c
@@ -2,11 +2,11 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] =
-    KEYMAP(PGUP, GRV ,  1   , 2   , 3   , 4     , 5       , NO   , NO     , 6      , 7      , 8   , 9   , 0   , MINS, EQL , GRV , BSPC, \
-           PGDN, TAB ,  Q   , W   , E   , R     , T       , NO   , NO     , Y      , U      , I   , O   , P   , LBRC, RBRC, BSLS, NO  , \
-           HOME, LCTL,  A   , S   , D   , F     , G       , NO   , NO     , H      , J      , K   , L   , SCLN, QUOT, NO  , ENT , NO  , \
-           END , LSFT,  Z   , X   , C   , V     , NO      , NO   , NO     , B      , N      , M   , COMM, DOT , SLSH, UP  , RSFT, NO  , \
-           INS , FN0 ,  LALT, LGUI, SPC , _VOLUP, _VOLDOWN, _MUTE, MS_BTN1, MS_BTN3, MS_BTN2, NO  , SPC , RGUI, LEFT, DOWN, RGHT, NO  ),
+    KEYMAP(PGUP, GRV ,  1   , 2   , 3   , 4     , 5       , NO   , 6      , 7      , 8      , 9   , 0   , MINS, EQL , GRV , BSPC, \
+           PGDN, TAB ,  Q   , W   , E   , R     , T       , NO   , NO     , Y      , U      , I   , O   , P   , LBRC, RBRC, BSLS, \
+           HOME, LCTL,  A   , S   , D   , F     , G       , NO   , NO     , H      , J      , K   , L   , SCLN, QUOT, NO  , ENT , \
+           END , LSFT,  Z   , X   , C   , V     , NO      , NO   , NO     , B      , N      , M   , COMM, DOT , SLSH, UP  , RSFT, \
+           INS , FN0 ,  LALT, LGUI, SPC , _VOLUP, _VOLDOWN, _MUTE, MS_BTN1, MS_BTN3, MS_BTN2, NO  , SPC , RGUI, LEFT, DOWN, RGHT ),
 
     [1] =
     KEYMAP(TRNS, ESC , F1   , F2  , F3  , F4    , F5      , TRNS , TRNS   , F6     , F7     , F8  , F9  , F10 , F11 , F12 , F13 , DEL , \
--- firmware/matrix.c
+++ firmware/matrix.c
@@ -135,15 +135,17 @@ uint8_t matrix_key_count(void)
 }
 
 /* Column pin configuration
- * col: 0   1   2   3   4   5   6   7   8
- * pin: B0  B1  B2  B3  F0  F1  F4  F5  F6
+ * col: 0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
+ * pin: B0  B1  B2  B3  B7  D0  D1  F0  F1  F4  F5  F6  F7  B6  B5  B4
  */
 static void  init_cols(void)
 {
-    DDRB  &= ~(1<<0 | 1<<1 | 1<<2 | 1<<3);
-    PORTB |=  (1<<0 | 1<<1 | 1<<2 | 1<<3);
-    DDRF  &= ~(1<<0 | 1<<1 | 1<<4 | 1<<5 | 1<<6);
-    PORTF |=  (1<<0 | 1<<1 | 1<<4 | 1<<5 | 1<<6);
+    DDRB  &= ~(1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<< 5 | 1<<6 | 1<<7);
+    PORTB |=  (1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<< 5 | 1<<6 | 1<<7);
+    DDRD  &= ~(1<<0 | 1<<1);
+    PORTD |=  (1<<0 | 1<<1);
+    DDRF  &= ~(1<<0 | 1<<1 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
+    PORTF |=  (1<<0 | 1<<1 | 1<<4 | 1<<5 | 1<<6 | 1<<7);
 }
 
 static matrix_row_t read_cols(void)
@@ -152,26 +154,31 @@ static matrix_row_t read_cols(void)
            (PINB&(1<<1) ? 0 : (1<<1)) |
            (PINB&(1<<2) ? 0 : (1<<2)) |
            (PINB&(1<<3) ? 0 : (1<<3)) |
-           (PINF&(1<<0) ? 0 : (1<<4)) |
-           (PINF&(1<<1) ? 0 : (1<<5)) |
-           (PINF&(1<<4) ? 0 : (1<<6)) |
-           (PINF&(1<<5) ? 0 : (1<<7)) |
-           (PINF&(1<<6) ? 0 : (1<<8));
+           (PINB&(1<<7) ? 0 : (1<<7)) |
+           (PIND&(1<<0) ? 0 : (1<<0)) |
+           (PIND&(1<<1) ? 0 : (1<<1)) |
+           (PINF&(1<<0) ? 0 : (1<<0)) |
+           (PINF&(1<<1) ? 0 : (1<<1)) |
+           (PINF&(1<<4) ? 0 : (1<<4)) |
+           (PINF&(1<<5) ? 0 : (1<<5)) |
+           (PINF&(1<<6) ? 0 : (1<<6)) |
+           (PINF&(1<<7) ? 0 : (1<<7)) |
+           (PINB&(1<<6) ? 0 : (1<<6)) |
+           (PINB&(1<<5) ? 0 : (1<<5)) |
+           (PINB&(1<<4) ? 0 : (1<<4));
 }
 
 /* Row pin configuration
- * row: 0   1   2   3   4   5   6   7   8   9
- * pin: B7  D0  D1  D2  D3  F7  B6  B5  B4  D7
+ * row: 0   1   2   3   4   5   6   7
+ * pin: D2  D3  C6  C7  D5  D7  D6  D4
  */
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRB  &= ~0b00001111;
-    PORTB &= ~0b00001111;
-    DDRD  &= ~0b11110001;
-    PORTD &= ~0b11110001;
-    DDRF  &= ~0b00000001;
-    PORTF &= ~0b00000001;
+    DDRC  &= ~0b00000011;
+    PORTC &= ~0b00000000;
+    DDRD  &= ~0b00111111;
+    PORTD &= ~0b00111111;
 }
 
 static void select_row(uint8_t row)
@@ -179,44 +186,36 @@ static void select_row(uint8_t row)
     // Output low(DDR:1, PORT:0) to select
     switch (row) {
         case 0:
-            DDRB  |=  (1<<7);
-            PORTB &= ~(1<<7);
-            break;
-        case 1:
-            DDRD  |=  (1<<0);
-            PORTD &= ~(1<<0);
-            break;
-        case 2:
-            DDRD  |=  (1<<1);
-            PORTD &= ~(1<<1);
-            break;
-        case 3:
             DDRD  |=  (1<<2);
             PORTD &= ~(1<<2);
             break;
-        case 4:
+        case 1:
             DDRD  |=  (1<<3);
             PORTD &= ~(1<<3);
             break;
+        case 2:
+            DDRC  |=  (1<<6);
+            PORTC &= ~(1<<6);
+            break;
+        case 3:
+            DDRC  |=  (1<<7);
+            PORTC &= ~(1<<7);
+            break;
+        case 4:
+            DDRD  |=  (1<<5);
+            PORTD &= ~(1<<5);
+            break;
         case 5:
-            DDRF  |=  (1<<7);
-            PORTF &= ~(1<<7);
-            break;
-        case 6:
-            DDRB  |=  (1<<6);
-            PORTB &= ~(1<<6);
-            break;
-        case 7:
-            DDRB  |=  (1<<5);
-            PORTB &= ~(1<<5);
-            break;
-        case 8:
-            DDRB  |=  (1<<4);
-            PORTB &= ~(1<<4);
-            break;
-        case 9:
             DDRD  |=  (1<<7);
             PORTD &= ~(1<<7);
+            break;
+        case 6:
+            DDRD  |=  (1<<6);
+            PORTD &= ~(1<<6);
+            break;
+        case 7:
+            DDRD  |=  (1<<4);
+            PORTD &= ~(1<<4);
             break;
     }
 }
